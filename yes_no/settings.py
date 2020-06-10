8144798dc5f510c517b878127712852b2cb86836
Colin J McFaul <colinjmcfaul@gmail.com>
2020-06-10 16:52:00 -0400

    commit before pushing to heroku

--- yes_no/settings.py
+++ yes_no/settings.py
@@ -16,14 +16,6 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
 HEROKU = os.environ.get('HEROKU', False)
 
 if not HEROKU:

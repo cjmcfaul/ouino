852161cf69f6b87e817d160d2254389160cc7c9a
Colin J McFaul <colinjmcfaul@gmail.com>
2020-06-10 17:27:50 -0400

    add more info to slack api views

--- slacks/api_views.py
+++ slacks/api_views.py
@@ -1,6 +1,18 @@
+from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+@csrf_exempt
+@api_view(['POST', ])
 def interactive_commands(request):
     print(request.data)
+    return Response(status=status.HTTP_200_OK)
 
-
+@csrf_exempt
+@api_view(['POST', ])
 def question(request):
     print(request.data)
+    return Response(status=status.HTTP_200_OK)

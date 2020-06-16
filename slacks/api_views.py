d906afda9e6cea745d66246d82d53f7b6359fc3f
Colin J McFaul <colinjmcfaul@gmail.com>
2020-06-15 23:53:29 -0400

    add celery

--- slacks/api_views.py
+++ slacks/api_views.py
@@ -55,7 +55,7 @@ def interactive_commands(request):
                 "response_type": "in_channel"
         })
         if question.reminder_time:
-            delivery = timezone.make_aware(datetime.datetime.fromtimestamp(float(data['container']['message_ts']))) + question.reminder_time
+            delivery = timezone.now() + question.reminder_time
             respond_notify.apply_async((question.public_id, ), eta=delivery)
     elif action_id == 'cancel_question':
         question = Question.objects.get(public_id=actions['value'])
